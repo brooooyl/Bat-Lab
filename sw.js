4d94abcc364fe28f08c26c389ab9fa2b09de68b6
brooooyl <yanromero055@gmail.com>
2026-09-19 09:34:50 -0300

    Update service worker cache and file list

--- sw.js
+++ sw.js
@@ -1,23 +1,57 @@
-const CACHE_NAME = "bat-lab-v1";
+const CACHE_NAME = "bat-lab-v2";
 
-const ARQUIVOS = [
+const FILES = [
     "./",
     "./index.html",
     "./style.css",
     "./app.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+
+    self.skipWaiting();
+
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(ARQUIVOS))
+            .then(cache => cache.addAll(FILES))
     );
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+
+            );
+
+        }).then(() => self.clients.claim())
+
+    );
+
 });
 
 self.addEventListener("fetch", event => {
+
     event.respondWith(
+
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(cached => {
+
+                return cached || fetch(event.request);
+
+            })
+
     );
+
 });
